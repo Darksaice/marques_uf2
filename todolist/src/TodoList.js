29bec1a7b3a2833ab62e837758e0358dfb3462c4
Darksaice <mitjansperezsara@gmail.com>
2021-04-09 14:35:19 +0200

    modificado todolist

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -4,20 +4,46 @@ import ListItem from './ListItem';
 
 class TodoList extends Component {
 	
+		constructor (props) {
+			super(props);
+
+		 let todo_tasks = [];
+		 
+		 this.state = {
+		 	items : todo_tasks
+		 };
+			this.addItem = this.addItem.bind(this);
+
+		}
+
 	addItem (e) {
 		e.preventDefault();
+		
+		let text_v = document.getElementById("text-task").value;
+
+		this.state.items.push({id:10, item:text_v});
+		console.log(this.state.items);
+
+		this.setState({
+			items: this.state.items
+		});
+
 	}
 	
 	render() {
+				
+		let lista = this.state.items.map( (todo_item) => {
+			return (<ListItem item={todo_item.item}/>);
+		
+		});
+		
 		return (
 		<div>	
 			<ul>
-				<ListItem item="Uno" />
-				<ListItem item="Dos" />
-				<ListItem item="Tres" />
+				{lista}
 			</ul>
 			<form onSubmit={this.addItem}>
-				<p><input type="text" /></p>
+				<p><input type="text" id="text-task" /></p>
 				<p><button type="submit">Añadir</button></p>
 			</form>
 		</div>
