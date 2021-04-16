13e43336c2bd87834d1a4b25ba5cc9dc9fe1193b
Darksaice <mitjansperezsara@gmail.com>
2021-04-16 14:33:33 +0200

    todo list y css

--- todolist/src/ListItem.js
+++ todolist/src/ListItem.js
@@ -1,9 +1,24 @@
  import React, { Component } from 'react';
 
  class ListItem  extends Component {
-   render() {
+   
+	 constructor (props){
+	 		super(props);
+
+			this.removeItem = this.removeItem.bind(this);
+	 
+	 }
+
+
+	 removeItem (){
+	 	this.props.parentRemove(this.props.id_item);
+	 }
+	 
+	 
+	 render() {
      return (
-       <li>{this.props.item} <button>X</button></li>
+       <li>{this.props.item} <button className="delete"
+			 			onClick={this.removeItem}>X</button></li>
      
    );
   }
--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -2,26 +2,34 @@ import React, { Component } from 'react';
 
 import ListItem from './ListItem';
 
+import './TodoList.css';
+
 class TodoList extends Component {
 	
 		constructor (props) {
 			super(props);
+
 
 		 let todo_tasks = [];
 		 
 		 this.state = {
 		 	items : todo_tasks
 		 };
+			
+			this.last_id = 0;
 			this.addItem = this.addItem.bind(this);
-
+			this.removeItem = this.removeItem.bind(this);
 		}
 
 	addItem (e) {
 		e.preventDefault();
 		
 		let text_v = document.getElementById("text-task").value;
+		document.getElementById("text-task").value = "";
+		document.getElementById("text-task").focus;
+		this.last_id++;
 
-		this.state.items.push({id:10, item:text_v});
+		this.state.items.push({id:this.last_id, item:text_v});
 		console.log(this.state.items);
 
 		this.setState({
@@ -29,23 +37,44 @@ class TodoList extends Component {
 		});
 
 	}
-	
-	render() {
+
+	removeItem (id_item) {
+		
+		console.log("Remove del parent"+id_item);
+		
+		for (let i = 0; i<this.state.items.length; i++){
+			if(this.state.items[i].id === id_item){
+				this.state.items.splice(i,1);
+				console.log("Borrando" +i);
+				break;
+			}
+		
+		}
+		this.setState({
+			items: this.state.items
+		});
+
+	}
+
+	render (){
 				
 		let lista = this.state.items.map( (todo_item) => {
-			return (<ListItem item={todo_item.item}/>);
+			return (<ListItem item={todo_item.item}
+								id_item={todo_item.id}
+								parentRemove={this.removeItem} />);
 		
 		});
 		
 		return (
 		<div>	
-			<ul>
-				{lista}
-			</ul>
+			<p>Num Items: NUM</p>
 			<form onSubmit={this.addItem}>
 				<p><input type="text" id="text-task" /></p>
 				<p><button type="submit">Añadir</button></p>
 			</form>
+			<ul>
+				{lista}
+			</ul>
 		</div>
 		);
 	}
